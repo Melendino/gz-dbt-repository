b77a0f39b051342d7c752c395c61d6a2b4a82dc5
Edwin Melendez Perez <edwin.1.melendez@gmail.com>
2024-02-07 01:31:11 +0000

    :D

--- models/models/intermediate/int_orders_operational.sql
+++ models/models/intermediate/int_orders_operational.sql
@@ -1,6 +1,7 @@
 SELECT
   o.orders_id
   ,o.date_date
+  ,ROUND(CAST(o.margin AS FLOAT64) + CAST(s.shipping_fee AS FLOAT64) - (CAST(s.logcost AS FLOAT64) + CAST(s.ship_cost AS FLOAT64)), 2) AS operational_margin
   ,o.quantity
   ,o.revenue
   ,o.purchase_cost
@@ -9,6 +10,6 @@ SELECT
   ,s.logcost
   ,s.ship_cost
 FROM {{ref("int_orders_margin")}} o
-LEFT JOIN {{ref("stg_raw__ship")}} s
+LEFT JOIN {{ref("stg_raw__ship")}} s 
     USING(orders_id)
 ORDER BY orders_id desc
