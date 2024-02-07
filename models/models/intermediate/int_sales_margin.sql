e5794c5d7d10ec23212a58b7ddf7da182ed68f07
Edwin Melendez Perez <edwin.1.melendez@gmail.com>
2024-02-07 00:12:44 +0000

    products

--- models/models/intermediate/int_sales_margin.sql
+++ models/models/intermediate/int_sales_margin.sql
@@ -1,12 +1,12 @@
 SELECT
-    product_id,
+    products_id,
     date_date,
     orders_id,
     revenue,
     quantity,
-    CAST(purchase_price AS FLOAT64) AS purchase_price,
-    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+    CAST(purchse_price AS FLOAT64) AS purchse_price,
+    ROUND(s.quantity*CAST(p.purchse_price AS FLOAT64),2) AS purchse_cost,
+    s.revenue - ROUND(s.quantity*CAST(p.purchse_price AS FLOAT64),2) AS margin
 FROM {{ref("stg_raw__sales")}} s
 LEFT JOIN {{ref("stg_raw__product")}} p
-        USING (product_id)
+        ON s.(product_id) = p.(products_id)
